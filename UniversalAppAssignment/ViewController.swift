9ec44215116109fb5ddf608aa04f26963a7433a8
Gokul <gokulakrishnanka@gmail.com>
2020-03-06 19:27:38 +0530

    Initial Commit

---
+++ UniversalAppAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalAppAssignment
+//
+//  Created by apple on 06/03/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
